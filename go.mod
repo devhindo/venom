cc2b380d030b9166015b5bbaad4e68e140f6c385
devhindo <lildevhind@gmail.com>
2024-12-20 13:45:50 +0200

    Add Gorilla Mux for routing and implement product handler in main.go
    Signed-off-by: devhindo <lildevhind@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/devhindo/venom
 
 go 1.22.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,60 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
+
+	mux := mux.NewRouter()
 	http.HandleFunc("/", helloGoHandler)
+
+	mux.HandleFunc("/products/{key}", productHundler)
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
 func helloGoHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello net/http!\n"))
+}
+
+func productHundler(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Query().Get("key")
+	if key == "" {
+		http.Error(w, "key is required", http.StatusBadRequest)
+		return
+	}
+
+	switch r.Method {
+	case http.MethodGet:
+		value, err := Get(key)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		w.Write([]byte(value))
+	case http.MethodPut:
+		value := r.URL.Query().Get("value")
+		if value == "" {
+			http.Error(w, "value is required", http.StatusBadRequest)
+			return
+		}
+		if err := Put(key, value); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte("OK"))
+	case http.MethodDelete:
+		if err := Delete(key); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte("OK"))
+	default:
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
 }
 
